735bd81a1de95921f58bd5788482e83a90edf05c
Uri Kalish <uri.kalish@hpe.com>
2017-08-24 21:13:17 +0300

    home view

--- website/home/home-view.js
+++ website/home/home-view.js
@@ -6,9 +6,20 @@ angular.module('mainApp').directive('homeView', function () {
 	};
 });
 
-angular.module('mainApp').controller('homeCtrl', function homeCtrl($scope) {
+angular.module('mainApp').controller('homeCtrl', function homeCtrl($scope, $interval) {
 
 	$scope.model = {
+		nextLaunchDate: new Date(2029,6,16,6,32,0,0),
+		timeToNextLaunch: 0
 	};
 
+	function updateTimeToNextLaunch() {
+		$scope.model.timeToNextLaunch = (($scope.model.nextLaunchDate.getTime() - (new Date()).getTime()) / 1000).toFixed(0);
+	}
+
+	updateTimeToNextLaunch();
+
+	$interval(function() {
+		updateTimeToNextLaunch();
+	}, 1000);
 });
