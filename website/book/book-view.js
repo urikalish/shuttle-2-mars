c963a03663b1ed227d462c570a705953f85d7717
Uri Kalish <uri.kalish@hpe.com>
2017-09-13 17:29:27 +0300

    book

--- website/book/book-view.js
+++ website/book/book-view.js
@@ -14,9 +14,12 @@ angular.module('mainApp').controller('bookCtrl', function bookCtrl($scope, $inte
 			{name: 'JUL 2029', date: new Date(Date.UTC(2029,6,16,13,32,0,0)), from: 'Cape Canaveral, Florida', to: 'Station 002, Mars'},
 			{name: 'AUG 2029', date: new Date(Date.UTC(2029,7,16,13,32,0,0)), from: 'Cape Canaveral, Florida', to: 'Station 003, Mars'},
 			{name: 'SEP 2029', date: new Date(Date.UTC(2029,8,16,13,32,0,0)), from: 'Cape Canaveral, Florida', to: 'Station 001, Mars'},
-			{name: 'OCT 2029', date: new Date(Date.UTC(2029,9,16,13,32,0,0)), from: 'Cape Canaveral, Florida', to: 'Station 001, Mars'},
-			{name: 'NOV 2029', date: new Date(Date.UTC(2029,10,16,13,32,0,0)), from: 'Cape Canaveral, Florida', to: 'Station 001, Mars'},
-			{name: 'DEC 2029', date: new Date(Date.UTC(2029,11,16,13,32,0,0)), from: 'Cape Canaveral, Florida', to: 'Station 001, Mars'}
+			{name: 'OCT 2029', date: new Date(Date.UTC(2029,9,16,13,32,0,0)), from: 'Cape Canaveral, Florida', to: 'Station 002, Mars'},
+			{name: 'NOV 2029', date: new Date(Date.UTC(2029,10,16,13,32,0,0)), from: 'Cape Canaveral, Florida', to: 'Station 003, Mars'},
+			{name: 'DEC 2029', date: new Date(Date.UTC(2029,11,16,13,32,0,0)), from: 'Cape Canaveral, Florida', to: 'Station 001, Mars'},
+			{name: 'JAN 2030', date: new Date(Date.UTC(2030,0,16,13,32,0,0)), from: 'Cape Canaveral, Florida', to: 'Station 002, Mars'},
+			{name: 'FEB 2030', date: new Date(Date.UTC(2030,1,16,13,32,0,0)), from: 'Cape Canaveral, Florida', to: 'Station 003, Mars'},
+			{name: 'MAR 2030', date: new Date(Date.UTC(2030,2,16,13,32,0,0)), from: 'Cape Canaveral, Florida', to: 'Station 001, Mars'}
 		],
 		selectedLaunch: null,
 		timeToLaunch: 0
@@ -26,7 +29,7 @@ angular.module('mainApp').controller('bookCtrl', function bookCtrl($scope, $inte
 		$scope.model.timeToLaunch = (($scope.model.selectedLaunch.date.getTime() - (new Date()).getTime()) / 1000).toFixed(0);
 	}
 
-	$scope.launchChanged = function launchChanged(launch) {
+	$scope.launchChanged = function launchChanged() {
 		updateTimeToLaunch();
 	};
 
@@ -35,5 +38,8 @@ angular.module('mainApp').controller('bookCtrl', function bookCtrl($scope, $inte
 			updateTimeToLaunch();
 		}
 	}, 1000);
+
+	$scope.model.selectedLaunch = $scope.model.launches[0];
+	$scope.launchChanged();
 
 });
